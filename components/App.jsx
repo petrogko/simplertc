1b6313e63c149578f1026f67b40242b13adb7567
Petro G <petro.gko@gmail.com>
2018-01-28 18:54:32 -0500

    Adding newChannels through socket

--- components/App.jsx
+++ components/App.jsx
@@ -9,13 +9,44 @@ class App extends Component{
     this.state = {
       channels: [],
       activeChannel: {},
-      messages: {}
+      messages: {},
+      connected: false
     };
+  }
+  componentDidMount() {
+    let ws = this.ws = new WebSocket('ws://echo.websocket.org');
+    ws.onmessage = this.message.bind(this);
+    ws.onopen = this.open.bind(this);
+    ws.onclose = this.close.bind(this);
+  }
+  message() {
+    const event = JSON.parse(e.data);
+    if (event.name == 'channel add') {
+      this.newChannel(event.data);
+    }
+  }
+  open(e) {
+    this.setState({connected:true});
+  }
+  close() {
+    this.setState({connected:false});
+  }
+  newChannel(channel) {
+    let {channels} = this.state;
+    channels.push(channel);
+    this.setState({channels});
   }
   addChannel(name){
     let {channels} = this.state;
-    channels.push({id: channels.length, name});
-    this.setState({channels});
+    let msg = {
+      name: 'channel add',
+      data: {
+        id: channels.length,
+        name
+      }
+    }
+
+    this.ws.send(JSON.stringify(msg) );
   }
   setChannel(activeChannel){
     this.setState({activeChannel});
